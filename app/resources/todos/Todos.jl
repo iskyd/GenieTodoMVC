ac3be14f30c216168b7294a0cb0f248a35d1c779
iskyd <careddumattia@gmail.com>
2022-09-02 09:57:00 +0200

    initial commit

---
+++ app/resources/todos/Todos.jl
@@ -0,0 +1,21 @@
+module Todos
+
+import SearchLight: AbstractModel, DbId
+import Base: @kwdef
+using SearchLight
+using TodoMVC.TodosValidator
+import SearchLight.Validation: ModelValidator, ValidationRule
+
+export Todo
+
+@kwdef mutable struct Todo <: AbstractModel
+  id::DbId = DbId()
+  todo::String = ""
+  completed::Bool = false
+end
+
+SearchLight.Validation.validator(::Type{Todo}) = ModelValidator([
+  ValidationRule(:todo, TodosValidator.not_empty)
+])
+
+end
